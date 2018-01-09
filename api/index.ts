8cfbca9b06126c00486ee3e137a7a3dfcfd2dbe9
Marc Vornetran <marc.vornetran@gmail.com>
2018-01-09 18:10:01 +0100

    Basic socket.io api

---
+++ api/index.ts
@@ -0,0 +1,28 @@
+import * as http from 'http';
+import * as socketIO from 'socket.io';
+import Socket = SocketIO.Socket;
+import { IncomingMessage, ServerResponse } from 'http';
+
+const app = http.createServer(serverHandler);
+const io = socketIO(app);
+
+io.on('connection', connectionHandler);
+app.listen(3001);
+console.log('Listening on port 3001');
+
+function serverHandler(request: IncomingMessage, response: ServerResponse) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Request-Method', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+
+    if (request.method === 'OPTIONS') {
+        response.writeHead(200);
+        response.end();
+        return;
+    }
+}
+
+function connectionHandler(socket: Socket) {
+
+}
