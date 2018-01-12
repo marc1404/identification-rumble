c84c36fad13a75727712c01f13cd6cb320de408f
Marc Vornetran <marc.vornetran@gmail.com>
2018-01-12 18:48:28 +0100

    Add placeholder evaluation page

---
+++ pages/evaluation.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="p-3">
+
+        <p class="lead text-danger">Not implemented yet!</p>
+
+        <scanner />
+
+    </div>
+</template>
+
+<script>
+import scannerComponent from '~/components/scanner.vue';
+
+export default {
+    name: 'Evaluation',
+    components: {
+        scanner: scannerComponent
+    },
+    head() {
+        return {
+            title: 'Evaluation'
+        };
+    }
+};
+</script>
