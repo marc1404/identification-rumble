400590a552ec3dbaf0456fb212addd1e1fe7106d
Marc Vornetran <marc.vornetran@gmail.com>
2018-01-12 23:07:11 +0100

    Added activity log for language change

--- pages/language.vue
+++ pages/language.vue
@@ -1,17 +1,34 @@
 <template>
     <div class="p-3">
 
-        <section class="mb-3">
-            <h1>Language</h1>
+        <div class="row">
+            <div class="col-12 col-sm-6">
 
-            <div class="custom-control custom-radio" v-for="language in languages">
-                <input :id="language.code" type="radio" name="languageRadios" class="custom-control-input" :value="language" v-model="selectedLanguage">
-                <label :for="language.code" class="custom-control-label">
-                    <flag :countryCode="language.code" />
-                    {{ language.name }}
-                </label>
+                <section class="mb-3">
+                    <h1>Language</h1>
+
+                    <div class="custom-control custom-radio" v-for="language in languages">
+                        <input :id="language.code" type="radio" name="languageRadios" class="custom-control-input" :value="language" v-model="selectedLanguage">
+                        <label :for="language.code" class="custom-control-label">
+                            <flag :countryCode="language.code" />
+                            {{ language.name }}
+                        </label>
+                    </div>
+                </section>
+
             </div>
-        </section>
+            <div class="col-12 col-sm-6 mb-3 mb-sm-0">
+
+                <h1>Activity</h1>
+
+                <samp>
+                    <span v-for="message in messages">
+                        {{ message }}<br>
+                    </span>
+                </samp>
+
+            </div>
+        </div>
 
         <section>
             <scanner :active="!!selectedLanguage" @passport="handlePassport" />
@@ -40,7 +57,8 @@ export default {
     data() {
         return {
             selectedLanguage: null,
-            languages: languageService.getLanguages()
+            languages: languageService.getLanguages(),
+            messages: []
         };
     },
     methods: {
@@ -49,6 +67,12 @@ export default {
                 passport.id,
                 this.selectedLanguage.code
             );
+
+            const message = `Changed language of passport #${passport.id} to ${
+                this.selectedLanguage.name
+            }`;
+
+            this.messages.push(message);
         }
     }
 };
