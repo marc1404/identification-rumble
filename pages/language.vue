277320bc4f40d53ab1541c5ef2a13866998169d7
Marc Vornetran <marc.vornetran@gmail.com>
2018-01-23 16:22:23 +0100

    Remove activity section from language scanner

--- pages/language.vue
+++ pages/language.vue
@@ -1,34 +1,17 @@
 <template>
     <div class="p-3">
 
-        <div class="row">
-            <div class="col-12 col-sm-6">
+        <section class="mb-3">
+            <h1>Language</h1>
 
-                <section class="mb-3">
-                    <h1>Language</h1>
-
-                    <div class="custom-control custom-radio" v-for="language in languages">
-                        <input :id="language.code" type="radio" name="languageRadios" class="custom-control-input" :value="language" v-model="selectedLanguage">
-                        <label :for="language.code" class="custom-control-label">
-                            <flag :countryCode="language.code" />
-                            {{ language.name }}
-                        </label>
-                    </div>
-                </section>
-
+            <div class="custom-control custom-radio" v-for="language in languages">
+                <input :id="language.code" type="radio" name="languageRadios" class="custom-control-input" :value="language" v-model="selectedLanguage">
+                <label :for="language.code" class="custom-control-label">
+                    <flag :countryCode="language.code" />
+                    {{ language.name }}
+                </label>
             </div>
-            <div class="col-12 col-sm-6 mb-3 mb-sm-0">
-
-                <h1>Activity</h1>
-
-                <samp>
-                    <span v-for="message in messages">
-                        {{ message }}<br>
-                    </span>
-                </samp>
-
-            </div>
-        </div>
+        </section>
 
         <section>
             <scanner @passport="handlePassport" />
@@ -57,8 +40,7 @@ export default {
     data() {
         return {
             selectedLanguage: null,
-            languages: languageService.getLanguages(),
-            messages: []
+            languages: languageService.getLanguages()
         };
     },
     methods: {
@@ -67,12 +49,6 @@ export default {
                 passport.id,
                 this.selectedLanguage.code
             );
-
-            const message = `Changed language of passport #${passport.id} to ${
-                this.selectedLanguage.name
-            }`;
-
-            this.messages.push(message);
         }
     }
 };
