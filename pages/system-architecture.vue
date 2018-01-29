3f0e6df983209e37bfe4b6bce524dbbf91dec2ba
Marc Vornetran <marc.vornetran@gmail.com>
2018-01-29 15:23:37 +0100

    Add system architecture page

---
+++ pages/system-architecture.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="p-3">
+        <h1>System Architecture</h1>
+        <img src="~/assets/system-architecture.png" class="img-fluid">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SystemArchitecture',
+    head() {
+        return {
+            title: 'System Architecture'
+        };
+    }
+};
+</script>
